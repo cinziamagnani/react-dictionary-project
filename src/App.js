f14a4f4ce15d5f8e75f9c9f16adf690f2fddb080
cinziamagnani <cinziamagnani@users.noreply.github.com>
2021-07-16 15:05:43 +0200

    setup basic search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import './App.css';
 import logo from "./logoshecodes-logo.png";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <img src={logo} alt="SheCodes logo" className="App-logo img-fluid"/>
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+        <small>
+        <a href="https://github.com/cinziamagnani/react-dictionary-project">
+          Open-source code
+        </a> by
+        <a href="https://cinziamagnani.com/">
+          Cinzia Magnani
+        </a>
+        </small>
+      </footer>
+      </div>
     </div>
   );
 }
