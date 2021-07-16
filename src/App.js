ca1560091ba7247871539e7793a38a65bbb22394
cinziamagnani <cinziamagnani@users.noreply.github.com>
2021-07-16 13:18:07 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import logo from "./logoshecodes-logo.png";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt="SheCodes logo" className="App-logo img-fluid"/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
